faf7024680c1bb0bf8fe1a70bf0f2477a5a1f11f
CristiStratulat <43218916+CristiStratulat@users.noreply.github.com>
2021-04-01 16:58:44 +0300

    Scrapping all the webistes and created a generic scraper class

---
+++ GenericWebScraper.cs
@@ -0,0 +1,52 @@
+﻿using OpenQA.Selenium.Chrome;
+using System.Text;
+
+namespace WebScrapper
+{
+    public class GenericWebScraper
+    {
+        public GenericWebScraper()
+        {
+
+        }
+        public static string WebScrap(string siteName, string URL, string XPathItem, string XPathPrice)
+        {
+            var builder = new StringBuilder();
+            builder.Append(siteName + System.Environment.NewLine);
+            var driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl(URL);
+
+            var itemNames = driver.FindElementsByXPath(XPathItem);
+            var itemPrices = driver.FindElementsByXPath(XPathPrice);
+
+            for(int i=0;i<itemNames.Count;i++)
+            {
+                if(siteName=="EMAG")
+                {
+                    builder.Append(Modify(itemPrices[i].GetAttribute("innerText").ToString()) + " " + itemNames[i].GetAttribute("innerText").ToString() + System.Environment.NewLine);
+                }
+                else
+                    builder.Append(itemPrices[i].GetAttribute("innerText").ToString()+" "+itemNames[i].GetAttribute("innerText").ToString()+System.Environment.NewLine);
+            }
+            return builder.ToString();
+        }
+        private static string Modify(string x)
+        {
+            var builder = new StringBuilder();
+            int count = 0;
+            foreach (char c in x)
+            {
+                builder.Append(c);
+                count++;
+                if (count == x.Length - 6)
+                {
+                    builder.Append(',');
+                }
+
+            }
+            return builder.ToString();
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,48 +2,50 @@
 using OpenQA.Selenium.Chrome;
 using Discord.Webhook;
 using System.Text;
-using System.Collections.Generic;
+using WebScrapper;
 
 namespace WebScraper
 {
     class Program
     {
-        private static string Modify(string x)
-        {
-            var builder = new StringBuilder();
-            int count = 0;
-            foreach (char c in x)
-            {
-                builder.Append(c);
-                count++;
-                if (count == x.Length - 6)
-                {
-                    builder.Append(',');
-                }
-
-            }
-            return builder.ToString();
-        }
+        
         static void Main(string[] args)
         {
             var txt = new StringBuilder();
-            txt.Append("EMAG" + System.Environment.NewLine);
+            string mediaGalaxy, emag, altex, flanco;
             DiscordWebhookClient ScrappyBot = new DiscordWebhookClient("https://discord.com/api/webhooks/826908590991474710/Q2wKMWry407JmnUyrT1wqZUkH7Enk1OS_bpnzoNqNB6nikime1CyRcEMhetPDWg_W_5x");
-            using (var driver = new ChromeDriver())
-                {
-                    driver.Navigate().GoToUrl("https://www.emag.ro/tablete/brand/apple/filter/ipad-f3557,ipad-air-4-v8668/conectivitate-f8661,fara-sim-v-7002211/capacitate-stocare-f8881,256-gb-v-4670700/c?ref=lst_quick_8661_-7002211");
-                
-                    var itemName = driver.FindElementsByClassName("product-title-zone");
-                    var priceValue = driver.FindElementsByClassName("product-new-price");
-      
-                for (int i = 0; i < priceValue.Count; i++)
-                    {
-                    txt.Append(Modify(priceValue[i].GetAttribute("innerText").ToString()) + " " + itemName[i].GetAttribute("innerText").ToString() + System.Environment.NewLine);
-                    }
-               
-            }
+         
+            mediaGalaxy = GenericWebScraper
+                .WebScrap("MEDIA GALAXY",
+                "https://mediagalaxy.ro/tablete/cpl/filtru/brand-3334/apple/capacitate-stocare-2967/256-gb/producator-procesor-2962/apple/dimensiune-ecran-inch-8660/10-5-11/conectivitate-3885/wi-fi/tip-procesor-2963/hexa-core/",
+                "//*[@class='Product-list-center']",
+                "//*[@class='Price-current']");
+            txt.Append(mediaGalaxy);
 
-            ScrappyBot.SendMessageAsync(txt.ToString());
+            emag = GenericWebScraper.
+                WebScrap("EMAG",
+                "https://www.emag.ro/tablete/brand/apple/filter/ipad-f3557,ipad-air-4-v8668/conectivitate-f8661,fara-sim-v-7002211/capacitate-stocare-f8881,256-gb-v-4670700/c?ref=lst_quick_8661_-7002211",
+                "//*[@class='product-title js-product-url']",
+                "//*[@class='product-new-price']"
+                );
+            txt.Append(emag);
+            
+            altex = GenericWebScraper.
+                WebScrap("ALTEX",
+                "https://altex.ro/tablete/cpl/filtru/brand-3334/apple/conectivitate-3885/wi-fi/tip-procesor-2963/hexa-core/capacitate-stocare-2967/256-gb/",
+                "//*[@class='Product-list-center']",
+                "//*[@class='Price-int']");
+            txt.Append(altex);
+            
+            flanco = GenericWebScraper.
+                 WebScrap("FLANCO",
+                 "https://www.flanco.ro/telefoane-tablete/tablete-si-accesorii/tablete-ios/filtre/conectivitate-tablete/fara_sim/model-procesor-telefoane/hexa_core/model-tableta/ipad_air/price/3500-.html",
+                 "//*[@class='produs-title']",
+                 "//*[@class='produs-price special-priceyso']");
+            txt.Append(flanco);
+            
+        
+           ScrappyBot.SendMessageAsync(txt.ToString());
 
         }
 
